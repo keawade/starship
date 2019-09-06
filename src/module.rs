0aa783471a9dc553f777cf5cf80b83994b4330d5
Nick Young <nick@nickwb.net>
2019-09-06 15:50:47 +1000

    chore: Re-organise module lists

--- src/module.rs
+++ src/module.rs
@@ -5,6 +5,7 @@ use ansi_term::{ANSIString, ANSIStrings};
 use std::fmt;
 
 // List of all modules
+// These are ordered alphabetically now
 pub const ALL_MODULES: &[&str] = &[
     "aws",
     #[cfg(feature = "battery")]
